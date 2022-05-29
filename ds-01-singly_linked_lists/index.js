b9f25e832b4ef8fb59d05aa05692b65434db45b9
architMahto <architmahto@gmail.com>
2022-05-29 11:06:30 -0400

    feat: implemented pop method from a singly linked list

--- ds-01-singly_linked_lists/index.js
+++ ds-01-singly_linked_lists/index.js
@@ -3,5 +3,10 @@ const SinglyLinkedList = require('./singlyLinkedList')
 const list = new SinglyLinkedList()
 list.push('Hello')
 list.push('Goodbye')
+list.push('!')
+
+list.print()
+
+list.pop()
 
 list.print()
--- ds-01-singly_linked_lists/singlyLinkedList.js
+++ ds-01-singly_linked_lists/singlyLinkedList.js
@@ -26,6 +26,29 @@ class SinglyLinkedList {
     this.length++
   }
 
+  pop() {
+    if (!this.head) return undefined
+
+    let current = this.head
+    let newTail = current
+
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+
+    return current
+  }
+
   print() {
     let current = this.head
 
