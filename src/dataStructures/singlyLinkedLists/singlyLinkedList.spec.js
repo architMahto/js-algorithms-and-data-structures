a812f438bf7bdb1969e4a462b970f165093c53aa
architMahto <architmahto@gmail.com>
2022-05-29 21:05:34 -0400

    implemennted push and pop methods for singly linked list

--- src/dataStructures/singlyLinkedLists/singlyLinkedList.spec.js
+++ src/dataStructures/singlyLinkedLists/singlyLinkedList.spec.js
@@ -2,7 +2,25 @@ const { expect } = require('chai')
 
 const SinglyLinkedList = require('./singlyLinkedList')
 
-describe.skip('Testing the Singly Linked List data structure', () => {
+describe('Testing the Singly Linked List data structure', () => {
+  let list, emptyList
+
+  beforeEach(() => {
+    emptyList = new SinglyLinkedList()
+    list = new SinglyLinkedList()
+
+    list.push(5)
+    list.push(2)
+    list.push(8)
+    list.push(14)
+    list.push(1)
+  })
+
+  afterEach(() => {
+    emptyList = null
+    list = null
+  })
+
   describe('init()', () => {
     let list = new SinglyLinkedList()
 
@@ -14,6 +32,46 @@ describe.skip('Testing the Singly Linked List data structure', () => {
       expect(list.head).to.be.null
       expect(list.tail).to.be.null
       expect(list.length).to.equal(0)
+    })
+  })
+
+  describe('pop()', () => {
+    it('should be defined', () => {
+      expect(list.pop).to.not.be.undefined
+    })
+
+    it('should not remove and return any value if the list is empty', () => {
+      const returnedNode = emptyList.pop()
+
+      expect(returnedNode).to.be.undefined
+    })
+
+    it('should remove and return the last value from a non-empty list', () => {
+      const returnedNode = list.pop()
+
+      expect(returnedNode.val).to.equal(1)
+      expect(list.length).to.equal(4)
+    })
+  })
+
+  describe('push()', () => {
+    it('should be defined', () => {
+      expect(list.push).to.not.be.undefined
+    })
+
+    it('should add a value to an empty list', () => {
+      emptyList.push(7)
+
+      expect(emptyList.head.val).to.equal(7)
+      expect(emptyList.tail.val).to.equal(7)
+      expect(emptyList.length).to.equal(1)
+    })
+
+    it('should add a value to the end of a list', () => {
+      list.push(4)
+
+      expect(list.tail.val).to.equal(4)
+      expect(list.length).to.equal(6)
     })
   })
 })
