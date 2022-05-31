133ac4876aa29fc01228165dc3d44a06633981a2
architMahto <architmahto@gmail.com>
2022-05-31 15:39:30 -0400

    implemented push and pop methods of stack

--- src/dataStructures/stacks/stack.js
+++ src/dataStructures/stacks/stack.js
@@ -11,6 +11,38 @@ class Stack {
     this.last = null
     this.size = 0
   }
+
+  pop() {
+    if (this.size === 0) return null
+
+    const returnedNode = this.first
+
+    if (this.size === 1) {
+      this.first = null
+      this.last = null
+    } else {
+      this.first = this.first.next
+    }
+
+    returnedNode.next = null
+    this.size--
+    return returnedNode.val
+  }
+
+  push(val) {
+    const newNode = new Node(val)
+
+    if (this.size === 0) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      const oldFirst = this.first
+      this.first = newNode
+      this.first.next = oldFirst
+    }
+
+    return ++this.size
+  }
 }
 
 module.exports = Stack
--- src/dataStructures/stacks/stack.spec.js
+++ src/dataStructures/stacks/stack.spec.js
@@ -3,17 +3,73 @@ const { expect } = require('chai')
 const Stack = require('./stack')
 
 describe('Testing the Stack data structure', () => {
+  let stack, emptyStack
+
+  beforeEach(() => {
+    stack = new Stack()
+    emptyStack = new Stack()
+
+    stack.push(5)
+    stack.push(2)
+    stack.push(8)
+    stack.push(14)
+    stack.push(1)
+  })
+
+  afterEach(() => {
+    stack = null
+    emptyStack = null
+  })
+
   describe('init()', () => {
-    let stack = new Stack()
+    const sampleStack = new Stack()
 
     it('should be defined', () => {
-      expect(stack).to.not.be.undefined
+      expect(sampleStack).to.not.be.undefined
     })
 
     it('should have first node, last node, and stack size initialized', () => {
-      expect(stack.first).to.be.null
-      expect(stack.last).to.be.null
-      expect(stack.size).to.equal(0)
+      expect(sampleStack.first).to.be.null
+      expect(sampleStack.last).to.be.null
+      expect(sampleStack.size).to.equal(0)
+    })
+  })
+
+  describe('pop()', () => {
+    it('should be defined', () => {
+      expect(stack.pop).to.not.be.undefined
+    })
+
+    it('should return null if list is empty', () => {
+      const result = emptyStack.pop()
+
+      expect(result).to.be.null
+    })
+
+    it('should remove and return the first value from the stack', () => {
+      const returnedNode = stack.pop()
+
+      expect(stack.size).to.equal(4)
+      expect(returnedNode).to.equal(1)
+    })
+  })
+
+  describe('push()', () => {
+    it('should be defined', () => {
+      expect(stack.push).to.not.be.undefined
+    })
+
+    it('should add a value to an empty stack', () => {
+      const size = emptyStack.push(9)
+
+      expect(size).to.equal(1)
+    })
+
+    it('should add a value to the beginning of a non-empty stack', () => {
+      const size = stack.push(9)
+
+      expect(size).to.equal(6)
+      expect(stack.first.val).to.equal(9)
     })
   })
 })
