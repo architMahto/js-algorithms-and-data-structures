16e66bf3c90e21125dc9a7d012fb1f0a54aaa4e4
architMahto <architmahto@gmail.com>
2022-05-31 11:15:16 -0400

    implemented push and pop methods for doubly linked lists

--- src/dataStructures/doublyLinkedLists/doublyLinkedList.js
+++ src/dataStructures/doublyLinkedLists/doublyLinkedList.js
@@ -12,6 +12,41 @@ class DoublyLinkedList {
     this.tail = null
     this.length = 0
   }
+
+  pop() {
+    if (!this.head) return undefined
+
+    let poppedNode = this.tail
+
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = poppedNode.prev
+      this.tail.next = null
+      poppedNode.prev = null
+    }
+
+    this.length--
+
+    return poppedNode
+  }
+
+  push(val) {
+    const newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+
+    this.length++
+    return this
+  }
 }
 
 module.exports = DoublyLinkedList
--- src/dataStructures/doublyLinkedLists/doublyLinkedList.spec.js
+++ src/dataStructures/doublyLinkedLists/doublyLinkedList.spec.js
@@ -3,17 +3,77 @@ const { expect } = require('chai')
 const DoublyLinkedList = require('./doublyLinkedList')
 
 describe('Testing the Doubly Linked List data structure', () => {
+  let list, emptyList, returnedNode
+
+  beforeEach(() => {
+    emptyList = new DoublyLinkedList()
+    list = new DoublyLinkedList()
+
+    list.push(5)
+    list.push(2)
+    list.push(8)
+    list.push(14)
+    list.push(1)
+  })
+
+  afterEach(() => {
+    emptyList = null
+    list = null
+    returnedNode = null
+  })
+
   describe('init()', () => {
-    let list = new DoublyLinkedList()
+    let sampleList = new DoublyLinkedList()
 
     it('should be defined', () => {
-      expect(list).to.not.be.undefined
+      expect(sampleList).to.not.be.undefined
     })
 
     it('should have head, tail, and length initialized', () => {
-      expect(list.head).to.be.null
-      expect(list.tail).to.be.null
-      expect(list.length).to.equal(0)
+      expect(sampleList.head).to.be.null
+      expect(sampleList.tail).to.be.null
+      expect(sampleList.length).to.equal(0)
+    })
+  })
+
+  describe('pop()', () => {
+    it('should be defined', () => {
+      expect(list.pop).to.not.be.undefined
+    })
+
+    it('should not remove and return any value if the list is empty', () => {
+      returnedNode = emptyList.pop()
+
+      expect(returnedNode).to.be.undefined
+    })
+
+    it('should remove and return the last value from a non-empty list', () => {
+      returnedNode = list.pop()
+
+      expect(returnedNode.val).to.equal(1)
+      expect(list.tail.val).to.equal(14)
+      expect(list.length).to.equal(4)
+    })
+  })
+
+  describe('push()', () => {
+    it('should be defined', () => {
+      expect(list.push).to.not.be.undefined
+    })
+
+    it('should add a value to an empty list', () => {
+      emptyList.push(7)
+
+      expect(emptyList.head.val).to.equal(7)
+      expect(emptyList.tail.val).to.equal(7)
+      expect(emptyList.length).to.equal(1)
+    })
+
+    it('should add a value to the end of a list', () => {
+      list.push(4)
+
+      expect(list.tail.val).to.equal(4)
+      expect(list.length).to.equal(6)
     })
   })
 })
