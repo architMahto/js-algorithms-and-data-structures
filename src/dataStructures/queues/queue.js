244a5517208ab9931755432500b465e92a49d5db
architMahto <architmahto@gmail.com>
2022-05-31 17:56:39 -0400

    implemented enqueue and dequeue methods for queue

--- src/dataStructures/queues/queue.js
+++ src/dataStructures/queues/queue.js
@@ -11,6 +11,37 @@ class Queue {
     this.last = null
     this.size = 0
   }
+
+  dequeue() {
+    if (this.size === 0) return null
+
+    const removedNode = this.first
+
+    if (this.size === 1) {
+      this.first = null
+      this.last = null
+    } else {
+      this.first = this.first.next
+    }
+
+    removedNode.next = null
+    this.size--
+    return removedNode.val
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val)
+
+    if (this.size === 0) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+
+    return ++this.size
+  }
 }
 
 module.exports = Queue
--- src/dataStructures/queues/queue.spec.js
+++ src/dataStructures/queues/queue.spec.js
@@ -8,6 +8,12 @@ describe('Testing the Queue data structure', () => {
   beforeEach(() => {
     queue = new Queue()
     emptyQueue = new Queue()
+
+    queue.enqueue(5)
+    queue.enqueue(2)
+    queue.enqueue(8)
+    queue.enqueue(14)
+    queue.enqueue(1)
   })
 
   afterEach(() => {
@@ -26,6 +32,47 @@ describe('Testing the Queue data structure', () => {
       expect(sampleQueue.first).to.be.null
       expect(sampleQueue.last).to.be.null
       expect(sampleQueue.size).to.equal(0)
+    })
+  })
+
+  describe('dequeue()', () => {
+    it('should be defined', () => {
+      expect(queue.dequeue).not.to.be.undefined
+    })
+
+    it('should not remove and return any value if the list is empty', () => {
+      returnedNode = emptyQueue.dequeue()
+
+      expect(returnedNode).to.be.null
+    })
+
+    it('should remove and return the first value from a non-empty list', () => {
+      returnedNode = queue.dequeue()
+
+      expect(returnedNode).to.equal(5)
+      expect(queue.first.val).to.equal(2)
+      expect(queue.size).to.equal(4)
+    })
+  })
+
+  describe('enqueue()', () => {
+    it('should be defined', () => {
+      expect(queue.enqueue).not.to.be.undefined
+    })
+
+    it('should add a value to an empty queue', () => {
+      emptyQueue.enqueue(7)
+
+      expect(emptyQueue.first.val).to.equal(7)
+      expect(emptyQueue.last.val).to.equal(7)
+      expect(emptyQueue.size).to.equal(1)
+    })
+
+    it('should add a value to the end of a non-empty queue', () => {
+      queue.enqueue(4)
+
+      expect(queue.last.val).to.equal(4)
+      expect(queue.size).to.equal(6)
     })
   })
 })
