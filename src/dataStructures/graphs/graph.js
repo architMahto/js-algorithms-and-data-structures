b244d530f6a033470c73c0d7888815138bbadcae
architMahto <architmahto@gmail.com>
2022-06-01 19:48:14 -0400

    iumplemented add edge method for graph

--- src/dataStructures/graphs/graph.js
+++ src/dataStructures/graphs/graph.js
@@ -3,6 +3,11 @@ class Graph {
     this.adjacencyList = {}
   }
 
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2)
+    this.adjacencyList[vertex2].push(vertex1)
+  }
+
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) {
       this.adjacencyList[vertex] = []
--- src/dataStructures/graphs/graph.spec.js
+++ src/dataStructures/graphs/graph.spec.js
@@ -8,6 +8,9 @@ describe('Testing the Binary Heap data structure', () => {
   beforeEach(() => {
     emptyGraph = new Graph()
     graph = new Graph()
+
+    const nodes = ['A', 'B', 'C', 'D', 'E']
+    nodes.forEach(node => graph.addVertex(node))
   })
 
   afterEach(() => {
@@ -27,12 +30,25 @@ describe('Testing the Binary Heap data structure', () => {
     })
   })
 
+  describe('addEdge()', () => {
+    it('should be defined', () => {
+      expect(graph.addEdge).to.not.be.undefined
+    })
+
+    it('should add a connection between two nodes in a graph', () => {
+      graph.addEdge('A', 'B')
+
+      expect(graph.adjacencyList['A']).to.include('B')
+      expect(graph.adjacencyList['B']).to.include('A')
+    })
+  })
+
   describe('addVertex()', () => {
     it('should be defined', () => {
       expect(graph.addVertex).to.not.be.undefined
     })
 
-    it('should set up a new node in the adjacency list', () => {
+    it('should add a new node to a graph', () => {
       emptyGraph.addVertex('A')
 
       expect(emptyGraph.adjacencyList).to.have.key('A')
