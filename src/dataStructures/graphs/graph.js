d6a00bcab8c8b227045f5fcef5ce6604284cfba8
architMahto <architmahto@gmail.com>
2022-06-01 19:35:24 -0400

    implemented add vertex method for graph

--- src/dataStructures/graphs/graph.js
+++ src/dataStructures/graphs/graph.js
@@ -2,6 +2,12 @@ class Graph {
   constructor() {
     this.adjacencyList = {}
   }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+    }
+  }
 }
 
 module.exports = Graph
--- src/dataStructures/graphs/graph.spec.js
+++ src/dataStructures/graphs/graph.spec.js
@@ -3,15 +3,40 @@ const { expect } = require('chai')
 const Graph = require('./graph')
 
 describe('Testing the Binary Heap data structure', () => {
+  let graph, emptyGraph
+
+  beforeEach(() => {
+    emptyGraph = new Graph()
+    graph = new Graph()
+  })
+
+  afterEach(() => {
+    emptyGraph = null
+    graph = null
+  })
+
   describe('init()', () => {
-    let graph = new Graph()
+    let sampleGraph = new Graph()
 
     it('should be defined', () => {
-      expect(graph).to.not.be.undefined
+      expect(sampleGraph).to.not.be.undefined
     })
 
     it('should have adjacency list initialized', () => {
-      expect(graph.adjacencyList).to.be.empty
+      expect(sampleGraph.adjacencyList).to.be.empty
+    })
+  })
+
+  describe('addVertex()', () => {
+    it('should be defined', () => {
+      expect(graph.addVertex).to.not.be.undefined
+    })
+
+    it('should set up a new node in the adjacency list', () => {
+      emptyGraph.addVertex('A')
+
+      expect(emptyGraph.adjacencyList).to.have.key('A')
+      expect(emptyGraph.adjacencyList['A']).to.deep.equal([])
     })
   })
 })
