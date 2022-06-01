c93c82bdf19aa8aea123aacab9c773130e6d33cc
architMahto <architmahto@gmail.com>
2022-05-31 22:47:23 -0400

    implemented find and insert methods for binary search trees

--- src/dataStructures/binarySearchTrees/binarySearchTree.js
+++ src/dataStructures/binarySearchTrees/binarySearchTree.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor(value) {
-    this.value = value
+  constructor(val) {
+    this.val = val
     this.left = null
     this.right = null
   }
@@ -9,6 +9,38 @@ class Node {
 class BinarySearchTree {
   constructor() {
     this.root = null
+  }
+
+  find(val) {
+    const _find = (val, node) => {
+      if (node === null) {
+        return undefined
+      } else if (node.val === val) {
+        return node
+      } else if (node.val < val) {
+        return _find(val, node.right)
+      } else {
+        return _find(val, node.left)
+      }
+    }
+
+    return _find(val, this.root)
+  }
+
+  insert(val) {
+    const _insert = (val, node) => {
+      if (node === null) {
+        node = new Node(val)
+      } else if (node.val < val) {
+        node.right = _insert(val, node.right)
+      } else {
+        node.left = _insert(val, node.left)
+      }
+
+      return node
+    }
+
+    this.root = _insert(val, this.root)
   }
 }
 
--- src/dataStructures/binarySearchTrees/binarySearchTree.spec.js
+++ src/dataStructures/binarySearchTrees/binarySearchTree.spec.js
@@ -3,6 +3,29 @@ const { expect } = require('chai')
 const { BinarySearchTree, Node } = require('./binarySearchTree')
 
 describe('Testing the Binary Search Tree data structure', () => {
+  let bst, noRootBst, foundNode
+
+  beforeEach(() => {
+    noRootBst = new BinarySearchTree()
+
+    bst = new BinarySearchTree()
+
+    bst.insert(15)
+    bst.insert(10)
+    bst.insert(20)
+    bst.insert(5)
+    bst.insert(7)
+    bst.insert(13)
+    bst.insert(18)
+    bst.insert(25)
+  })
+
+  afterEach(() => {
+    noRootBst = null
+    bst = null
+    foundNode = null
+  })
+
   describe('init()', () => {
     let sampleBst = new BinarySearchTree()
     sampleBst.root = new Node(15)
@@ -14,7 +37,61 @@ describe('Testing the Binary Search Tree data structure', () => {
     it('should have root node initialized', () => {
       expect(sampleBst.root.left).to.be.null
       expect(sampleBst.root.right).to.be.null
-      expect(sampleBst.root.value).to.equal(15)
+      expect(sampleBst.root.val).to.equal(15)
+    })
+  })
+
+  describe('find()', () => {
+    it('should be defined', () => {
+      expect(bst.find).to.not.be.undefined
+    })
+
+    it('should return null if value is not found in tree', () => {
+      foundNode = bst.find(40)
+
+      expect(foundNode).to.be.undefined
+    })
+
+    it('should return node if value is found on the right side of the tree', () => {
+      foundNode = bst.find(20)
+
+      expect(foundNode).to.not.be.undefined
+      expect(foundNode.val).to.equal(20)
+    })
+
+    it('should return node if value is found on the left side of the tree', () => {
+      foundNode = bst.find(5)
+
+      expect(foundNode).to.not.be.undefined
+      expect(foundNode.val).to.equal(5)
+    })
+  })
+
+  describe('insert()', () => {
+    it('should be defined', () => {
+      expect(bst.insert).to.not.be.undefined
+    })
+
+    it('set the root for a tree with no root', () => {
+      noRootBst.insert(15)
+
+      expect(noRootBst.root.val).to.equal(15)
+    })
+
+    it('should enter a value lesser than the root to the left of the root', () => {
+      bst.insert(2)
+      const expectedNode = bst.root.left.left.left
+
+      expect(expectedNode).to.not.be.undefined
+      expect(expectedNode.val).to.equal(2)
+    })
+
+    it('should enter a value greater than the root to the right of the root', () => {
+      bst.insert(27)
+      const expectedNode = bst.root.right.right.right
+
+      expect(expectedNode).to.not.be.undefined
+      expect(expectedNode.val).to.equal(27)
     })
   })
 })
